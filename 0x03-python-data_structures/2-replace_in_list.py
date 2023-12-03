b231e498d61883618d469af842e14b6819f4ac32
Duaa74 <duaasayed74@gmail.com>
2023-12-03 21:57:41 +0300

    h

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    my_list[3] = 9
+    if idx >= 0 or idx >= len(my_list):
+        return my_list
+    else:
+        return new_list
