9f596a9d43cd26c8f920c1a2ee402f8dc8d6216d
Duaa74 <duaasayed74@gmail.com>
2023-12-07 03:53:46 +0300

    pt

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+
+    sum_values = 0
+    sum_weights = 0
+
+    for score, weight in my_list:
+        sum_values += score * weight
+        sum_weights += weight
+
+    if sum_weights == 0:
+        return 0
+
+    return sum_values / sum_weights
