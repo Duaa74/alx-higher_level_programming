68d0ae56fddb447c72c3e31ad0ac701972ab9737
Duaa74 <duaasayed74@gmail.com>
2023-12-20 03:05:18 +0300

    hh

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""sqare module"""
+
+
+class Square:
+    """define a square"""
+
+    def __init__(self, size):
+        """constractor.
+
+        args:
+                        size: size of the square
+        """
+        self.__size = size
